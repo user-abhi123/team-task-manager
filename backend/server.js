34fefb27f5bc8be61f18a059bb62a9f8f30d0a25
user-abhi123 <abhisheksingh506315@gamil.com>
2026-05-22 02:02:41 +0530

    fixed railway deployment

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,33 @@
 require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
+
 const connectDB = require("./config/db");
 
 const app = express();
 
+// DB CONNECT
 connectDB();
 
+// MIDDLEWARE
 app.use(cors());
 app.use(express.json());
 
-// routes
+// ROUTES
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/projects", require("./routes/projectRoutes"));
 app.use("/api/tasks", require("./routes/taskRoutes"));
-
-// ✅ ADD THIS
 app.use("/api/attendance", require("./routes/attendanceRoutes"));
 
-app.listen(5000, () => console.log("Server running on 5000"));
+// TEST ROUTE
+app.get("/", (req, res) => {
+  res.send("API Running...");
+});
+
+// ✅ IMPORTANT FOR RAILWAY
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});

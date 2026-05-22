5377b47181d95ccd6db0befac2c98d2c59078deb
user-abhi123 <abhisheksingh506315@gamil.com>
2026-05-22 16:48:13 +0530

    fixed users api

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,32 +1,57 @@
 const router = require("express").Router();
 
+const jwt = require("jsonwebtoken");
+
+const User = require("../models/User");
+
 const {
   signup,
   login,
 } = require("../controllers/authController");
 
-const auth = require("../middleware/auth");
-
-const User = require("../models/User");
 
 // REGISTER
 router.post("/signup", signup);
 
+
 // LOGIN
 router.post("/login", login);
 
-// GET ALL USERS
-router.get("/users", auth, async (req, res) => {
+
+// GET USERS
+router.get("/users", async (req, res) => {
+
   try {
+
+    const token = req.header("Authorization");
+
+    if (!token) {
+      return res.status(401).json({
+        msg: "No token",
+      });
+    }
+
+    const actualToken = token.split(" ")[1];
+
+    jwt.verify(
+      actualToken,
+      process.env.JWT_SECRET
+    );
+
     const users = await User.find().select("-password");
 
     res.json(users);
 
   } catch (err) {
+
+    console.log(err);
+
     res.status(500).json({
       msg: "Server error",
     });
+
   }
+
 });
 
 module.exports = router;
